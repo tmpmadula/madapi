14f1d712345c4b0c6e5d517fa3bd8d6916e96197
tmpmadula <tmpmadula@gmail.com>
2022-05-07 21:42:07 +0200

    seed

--- src/users/dto/update-user.input.ts
+++
@@ -1,5 +0,0 @@
-import { InputType, OmitType, PartialType } from '@nestjs/graphql';
-import { CreateUserInput } from './create-user.input';
-
-@InputType()
-export class UpdateUserInput extends PartialType() {}
